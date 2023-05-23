e77fc3ee504b35955d28fb4ebafd0e4dab9c8dd2
Phuc Hoi <nphoi.toank28cltt@gmail.com>
2023-05-23 21:06:13 +0700

    convert index page to hbs

--- index.js
+++ index.js
@@ -3,18 +3,31 @@
 const express = require("express");
 const app = express();
 
-const path = require("path");
-
 const PORT = process.env.PORT || 3000;
+
+const path = require("path");
+const expressHandlebars = require("express-handlebars");
 
 //Cau hinh static folder
 //Can phai de file index.js o thu muc goc
-console.log(__dirname);
-app.use("/static", express.static(path.join(__dirname, "/public")));
+app.use(express.static(__dirname + "/public"));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello to Eshop");
-// });
+//Dinh nghia view engine
+app.engine(
+  "hbs",
+  expressHandlebars.engine({
+    layoutsDir: path.join(__dirname, "/views/layouts"),
+    partialsDir: path.join(__dirname, "/views/partials"),
+    extname: "hbs",
+    defaultLayout: "layout",
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
