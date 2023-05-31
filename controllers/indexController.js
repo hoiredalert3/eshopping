da380600ebe2252b21296c495005e1d6dfa6d668
Phuc Hoi <nphoi.toank28cltt@gmail.com>
2023-06-01 00:44:25 +0700

    fetch categories from database and render to index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,15 +4,21 @@ const controller = {};
 const models = require("../models");
 
 controller.showHomePage = async (req, res) => {
-  let brands = [];
-  const data = await models.Brand.findAll().then((brandsData) => {
-    brandsData.forEach((brand) => {
-      brands.push(brand.dataValues);
-    });
-    console.log(brandsData);
-    // console.log(brands);
-    res.render("index", { brands: brandsData });
+  const [brandsData, categoriesData] = await Promise.all([
+    models.Brand.findAll(),
+    models.Category.findAll(),
+  ]);
+
+  const arr1 = categoriesData.splice(2, 2);
+  const arr2 = categoriesData.splice(1, 1);
+  const newCategoriesData = [categoriesData, arr1, arr2];
+  console.log(newCategoriesData);
+
+  res.locals.categoriesData = newCategoriesData;
+  categoriesData.forEach((categories) => {
+    console.log(categories);
   });
+  res.render("index", { brands: brandsData });
 };
 
 controller.showPage = (req, res, next) => {
