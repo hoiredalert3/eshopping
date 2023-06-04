e8260db2a28feac0930eb768ac4ff47a5bec3d59
Phuc Hoi <nphoi.toank28cltt@gmail.com>
2023-06-04 20:17:22 +0700

    implement view cart details

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,20 +1,25 @@
-"use strict";
+"use strict"
 
-const models = require("../models");
+const models = require("../models")
 
-let controller = {};
+let controller = {}
 
 controller.add = async (req, res) => {
-  const id = isNaN(req.body.id) ? 0 : parseInt(Math.max(0, req.body.id));
+  const id = isNaN(req.body.id) ? 0 : parseInt(Math.max(0, req.body.id))
   const quantity = isNaN(req.body.quantity)
     ? 0
-    : parseInt(Math.max(0, req.body.quantity));
+    : parseInt(Math.max(0, req.body.quantity))
 
-  const product = await models.Product.findByPk(id);
+  const product = await models.Product.findByPk(id)
   if (product) {
-    req.session.cart.add(product, quantity);
+    req.session.cart.add(product, quantity)
   }
-  return res.json({ quantity: req.session.cart.quantity });
-};
+  return res.json({ quantity: req.session.cart.quantity })
+}
 
-module.exports = controller;
+controller.showCart = async (req, res) => {
+  res.locals.cart = req.session.cart.getCart()
+  return res.render("cart")
+}
+
+module.exports = controller
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,17 +1,20 @@
-"use strict";
-const express = require("express");
-const router = express.Router();
+"use strict"
+const express = require("express")
+const router = express.Router()
 
-const controller = require("../controllers/productsController");
-const cartController = require("../controllers/cartController");
+const controller = require("../controllers/productsController")
+const cartController = require("../controllers/cartController")
 
 //Show list of products
-router.get("/", controller.getData, controller.showProducts);
+router.get("/", controller.getData, controller.showProducts)
+
+//Show cart details
+router.get("/cart", cartController.showCart)
 
 //Show product details
-router.get("/:id", controller.getData, controller.showDetails);
+router.get("/:id", controller.getData, controller.showDetails)
 
 //Post route to add product
-router.post("/cart", cartController.add);
+router.post("/cart", cartController.add)
 
-module.exports = router;
+module.exports = router
