822ff81aa0a6eb4eee460a88d22d757f237035a4
Phuc Hoi <nphoi.toank28cltt@gmail.com>
2023-06-07 17:36:19 +0700

    add validation for account register

--- public/js/20120291.js
+++ public/js/20120291.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 const addCart = async (id, quantity) => {
   try {
@@ -6,17 +6,31 @@ const addCart = async (id, quantity) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Accept": "application/json",
+        Accept: "application/json",
       },
       body: JSON.stringify({ id, quantity }),
-    })
+    });
 
-    const result = await response.json()
+    const result = await response.json();
     document.getElementById(
       "cart-quantity"
-    ).textContent = `(${result.quantity})`
+    ).textContent = `(${result.quantity})`;
     // console.log("Success:", result)
   } catch (error) {
-    console.error("Error:", error)
+    console.error("Error:", error);
   }
-}
+};
+
+const checkConfirmPassword = (formId) => {
+  const password = document.querySelector(`#${formId} [name=password]`);
+  const confirmPassword = document.querySelector(
+    `#${formId} [name=confirmPassword]`
+  );
+
+  if (password.value != confirmPassword.value) {
+    confirmPassword.setCustomValidity("Confirm password does not match");
+    confirmPassword.reportValidity();
+  } else {
+    confirmPassword.setCustomValidity("");
+  }
+};
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -27,6 +27,36 @@ router.post(
 
 router.get("/logout", controller.logout);
 
-router.post("/register", controller.register);
+router.post(
+  "/register",
+  body("firstName").trim().notEmpty().withMessage("First name is required"),
+  body("lastName").trim().notEmpty().withMessage("Last name is required"),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required!")
+    .isEmail()
+    .withMessage("Invalid email address"),
+  body("password").trim().notEmpty().withMessage("Password is required"),
+  body("password")
+    .matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)
+    .withMessage(
+      "Password must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
+    ),
+  body("confirmPassword").custom((confirmPassword, { req }) => {
+    if (confirmPassword != req.body.password) {
+      throw new Error("Confirm password does not match");
+    }
+    return true;
+  }),
+  (req, res, next) => {
+    const msg = getErrorMsg(req);
+    if (msg) {
+      return res.render("login", { registerMessage: msg });
+    }
+    next();
+  },
+  controller.register
+);
 
 module.exports = router;
