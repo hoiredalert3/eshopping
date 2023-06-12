07239eb2f8491c1a4168b752766e9223e3ec319b
Phuc Hoi <nphoi.toank28cltt@gmail.com>
2023-06-12 18:07:56 +0700

    implement checkout

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,19 +1,27 @@
-"use strict"
+"use strict";
 
-const models = require("../models")
+const models = require("../models");
 
-let controller = {}
+let controller = {};
 
 controller.checkOut = async (req, res) => {
-  const userId = req.user.id
-}
+  if (req.session.cart.quantity > 0) {
+    const userId = req.user.id;
+    const addresses = await models.Address.findAll({ where: { userId } });
+
+    res.locals.addresses = addresses;
+    res.locals.cart = req.session.cart.getCart();
+    return res.render("checkout");
+  }
+  res.redirect("/products");
+};
 
 controller.placeOrders = async (req, res) => {
-  const userId = req.user.id
-}
+  const userId = req.user.id;
+};
 
 controller.saveOrders = async (req, res, status) => {
-  const userId = req.user.id
-}
+  const userId = req.user.id;
+};
 
-module.exports = controller
+module.exports = controller;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,14 +1,17 @@
-"use strict"
-const express = require("express")
-const router = express.Router()
-// const userController = require("../controllers/usersController")
-const { body, validationResult } = require("express-validator")
-const authController = require("../controllers/authController")
+"use strict";
 
-router.use(authController.isLoggedIn)
+const express = require("express");
+const router = express.Router();
+const usersController = require("../controllers/usersController");
+const { body, validationResult } = require("express-validator");
+const authController = require("../controllers/authController");
+
+router.use(authController.isLoggedIn);
 
 router.get("/my-account", (req, res) => {
-  res.render("my-account")
-})
+  res.render("my-account");
+});
 
-module.exports = router
+router.get("/checkout", usersController.checkOut);
+
+module.exports = router;
